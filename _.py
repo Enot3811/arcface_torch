e474e06f50b527294bd443a3db5c07ee7d46b2a3
Enot3811 <enot3811@gmail.com>
2023-04-26 17:41:49 +0300

    Function for showing grid of image pieces

--- _.py
+++ _.py
@@ -1,5 +1,22 @@
 import torch
 import numpy as np
+def show_grid(
+    arr: np.ndarray,
+    h: int,
+    w: int,
+    size: Tuple[float, float] = (20.0, 20.0)
+):
+    fig, axs = plt.subplots(h, w)
+    fig.set_size_inches(*size, forward=True)
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0, hspace=0)
+    for i in range(arr.shape[0]):
+        row = i // w
+        column = i % w
+        axs[row][column].get_yaxis().set_visible(False)
+        axs[row][column].get_xaxis().set_visible(False)
+        axs[row][column].imshow(arr[i])
+    return fig, axs
 
 label = np.array([5.4], dtype=np.float32)[0]
 label = label.astype(np.int32)
