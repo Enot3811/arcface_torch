b4fede04e9c6e5bf0a785719551ae9b92ee55f96
Enot3811 <enot3811@gmail.com>
2023-04-27 16:01:22 +0300

    Replaced function by imports

--- infer_all_img.py
+++ infer_all_img.py
@@ -6,7 +6,8 @@ import cv2
 import torch
 from tqdm import tqdm
 
-from backbones import get_model
+from model_tools import load_model
+from image_tools import load_images
 
 
 def main(
@@ -37,46 +38,6 @@ def main(
     
     results = np.array(all_results)
     np.save(dset_path / 'results.npy', results)
-
-
-def load_model(model_name: str, checkpoint_path: Path) -> torch.nn.Module:
-    """
-    Load a model from a checkpoint for an inference.
-
-    Args:
-        model_name (str): A name of the model.
-        checkpoint_path (Path): A path to the checkpoint.
-
-    Returns:
-        torch.nn.Module: _description_
-    """    
-    net = get_model(model_name, fp16=False)
-    net.load_state_dict(torch.load(checkpoint_path))
-    net.eval()
-    return net
-
-
-def load_images(image_paths: List[Path]) -> torch.Tensor:
-    """
-    Load and prepare images to pass into a network.
-
-    Args:
-        image_paths (List[Path]): Paths to the needed images.
-
-    Returns:
-        torch.Tensor: Loaded and processed images with shape
-        `[num_images, c, h, w].`
-    """    
-    images = []
-    for img in image_paths:
-        img = cv2.imread(str(img))
-        img = cv2.resize(img, (112, 112))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = np.transpose(img, (2, 0, 1))
-        img = torch.from_numpy(img).unsqueeze(0).float()
-        img.div_(255).sub_(0.5).div_(0.5)
-        images.append(img)
-    return torch.cat(images)
 
 
 def infer_img(img: Path, model: torch.nn.Module) -> np.ndarray:
