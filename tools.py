3805d3e2e2b1ab2008fe83904b702964ce0ace0c
Enot3811 <enot3811@gmail.com>
2023-04-27 15:42:09 +0300

    Read image func

--- tools.py
+++ tools.py
@@ -1,4 +1,40 @@
+from typing import Union
+from pathlib import Path
+
 import numpy as np
+import cv2
+
+
+def read_image(path: Union[Path, str], grayscale: bool = False) -> np.ndarray:
+    """
+    Read image to numpy array.
+    Parameters
+    ----------
+    path : Union[Path, str]
+        Path to image file
+    grayscale : bool, optional
+        Whether read image in grayscale, by default False
+    Returns
+    -------
+    np.ndarray
+        Array containing read image.
+    Raises
+    ------
+    FileNotFoundError
+        Did not find image.
+    ValueError
+        Image reading is not correct.
+    """
+    if isinstance(path, str):
+        path = Path(path)
+    if not path.exists():
+        raise FileNotFoundError(f'Did not find image {path}.')
+    flag = cv2.IMREAD_GRAYSCALE if grayscale else cv2.IMREAD_COLOR
+    img = cv2.imread(str(path), flag)
+    if img is None:
+        raise ValueError('Image reading is not correct.')
+    img = cv2.cv2tColor(img, cv2.COLOR_BGR2RGB)
+    return img
 
 
 def get_sliding_windows(
