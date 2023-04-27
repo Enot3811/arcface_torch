4250b5c3b95145a7e9eb93a284eb4222edd16e24
Enot3811 <enot3811@gmail.com>
2023-04-27 15:34:41 +0300

    Created a separate function for cutting image into windows

--- _.py
+++ _.py
@@ -13,20 +13,14 @@ from inference import inference
 def intersect_dicts(da, db, exclude=()):
     # Dictionary intersection of matching keys and shapes,
     # omitting 'exclude' keys, using da values
-    return {
+    intersect = {
         k: v
         for k, v in da.items()
         if (k in db and not any(x in k for x in exclude)
             and v.shape == db[k].shape)
     }
-
-
-def load_and_infer():
-    checkpoint_path = 'checkpoints/backbone.pth'
-    model_name = 'r50'
-    images = ['../data/test_satellite_112x112.png']
-
-    results = [inference(checkpoint_path, model_name, img) for img in images]
+    print(len(da), len(db), len(intersect))
+    return intersect
 
 
 def show_grid(
@@ -46,6 +40,39 @@ def show_grid(
         axs[row][column].get_xaxis().set_visible(False)
         axs[row][column].imshow(arr[i])
     return fig, axs
+
+
+def get_sliding_windows(
+    source_image: np.ndarray,
+    h_win: int,
+    w_win: int,
+    stride: int
+) -> np.ndarray:
+    """
+    Cut a given image into windows with defined shapes and stride.
+
+    Args:
+        source_image (np.ndarray): The original image.
+        h_win (int): Height of the windows.
+        w_win (int): Width of the windows.
+        stride (int): The stride of the sliding windows.
+
+    Returns:
+        np.ndarray: The cut image with shape `[num_windows, h_win, w_win, c]`.
+    """    
+    w, h, c = source_image.shape
+
+    x_indexer = (
+        np.expand_dims(np.arange(w_win), 0) +
+        np.expand_dims(np.arange(w - w_win - 1, step=stride), 0).T
+    )
+    y_indexer = (
+        np.expand_dims(np.arange(h_win), 0) +
+        np.expand_dims(np.arange(h - h_win - 1, step=stride), 0).T
+    )
+    windows = source_image[x_indexer][:, :, y_indexer].swapaxes(1, 2)
+    windows = windows.reshape(-1, w_win, h_win, c)
+    return windows
 
 
 def create_augmented_images():
