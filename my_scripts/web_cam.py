2a93e2d8c6842697d3a4f0532085c56ed33d3da3
Enot3811 <enot3811@gmail.com>
2023-06-07 12:30:15 +0300

    Добавлено удаление и создание папки для кадров с камеры

--- my_scripts/web_cam.py
+++ my_scripts/web_cam.py
@@ -13,21 +13,26 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) # Ширина кадров в видеопотоке.
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 960) # Высота кадров в видеопотоке.
 
+    out_dir = Path(__file__).parents[2] / 'camera_out'
+    # Если ранее уже существовала такая директория, то удаляем её
+    if out_dir.exists():
+        shutil.rmtree(out_dir)
+    # и создаём новую пустую
+    out_dir.mkdir(exist_ok=True, parents=True)
+
     count = 0
     while True:
         ret, img = cap.read()
         print(ret)
         cy = img.shape[0] // 2
         cx = img.shape[1] // 2
-        win = 120 # 224
+        win = 224  # Вырезаемой зоны из получаемого кадра
+        # Вырезается из середины
         img = img[cy - win // 2:cy + win // 2, cx - win // 2:cx + win // 2]
-        img = resize_image(img, (224, 224))
+        if win != 224:  # Если не совпадает с размером сети, то ресайз
+            img = resize_image(img, (224, 224))
         
         cv2.imshow('camera', img)
-        out_dir = Path(__file__).parents[2] / 'camera_out'
-        if out_dir.exists():
-            shutil.rmtree(out_dir)
-        out_dir.mkdir(exist_ok=True, parents=True)
         img_pth = out_dir / (str(count) + '.jpg')
         cv2.imwrite(str(img_pth), img)
         if cv2.waitKey(10) == 27: # Клавиша Esc
