d5c2021938dddc227285557c9163e7f577e788f1
Enot3811 <enot3811@gmail.com>
2023-06-08 12:50:52 +0300

    Добавлено отображение классифицированного региона на карте

--- my_scripts/online_predict.py
+++ my_scripts/online_predict.py
@@ -18,7 +18,8 @@ import sys
 sys.path.append(str(Path(__file__).parents[1]))
 from my_utils.np_tools import normalize, classify_samples, normalize
 from my_utils.model_tools import load_model, preprocess_model_input
-from my_utils.image_tools import read_image
+from my_utils.image_tools import read_image, resize_image
+from my_utils.grid_tools import ClassesGrid
 
 
 def main(**kwargs):
@@ -29,6 +30,8 @@ def main(**kwargs):
     file_ext: str = kwargs['files_extension']
     show_time: bool = kwargs['time_info']
     cls_img_dir: Path = kwargs['cls_images']
+    source_map_pth: Path = kwargs['map_path']
+    step: int = kwargs['step_px']
     
     # (n_cls, n_samples, embed), или (n_cls, 1, embed), или (n_cls, embed)
     embeddings: np.ndarray = np.load(embeddings_pth)
@@ -36,6 +39,10 @@ def main(**kwargs):
     if len(embeddings.shape) == 3:
         embeddings = embeddings.mean(axis=1)
     
+    if source_map_pth is not None:
+        source_map = read_image(source_map_pth)
+        cls_grid = ClassesGrid(*source_map.shape[:2], step)
+
     with torch.no_grad():
         model = load_model(model_name, model_path)
         img_paths = set(input_dir.glob('*.' + file_ext))
@@ -71,10 +78,25 @@ def main(**kwargs):
                 if show_time:
                     end = time.time() - start
                     print('{:.2f} секунд'.format(end))
+                if source_map_pth is not None:
+                    map_selected = cls_grid.show_selected_reg(
+                        source_map, predicted_cls)
+                    # Изменить размеры, чтобы отобразить в окне
+                    show_img_w = 1000  # Ширина изображения в окне
+                    h, w = map_selected.shape[:2]
+                    resize_factor = show_img_w / w
+                    show_img_h = int(h * resize_factor)
+                    map_selected = resize_image(
+                        map_selected, (show_img_h, show_img_w))
+
+                    cv2.imshow('Predicted region',
+                               cv2.cvtColor(map_selected, cv2.COLOR_RGB2BGR))
+                    if cv2.waitKey(10) == 27:  # Клавиша Esc
+                        break
                 if cls_img_dir is not None:
                     cls_img = cv2.imread(
                         str(cls_img_dir / f's{predicted_cls}.jpg'))
-                    cv2.imshow('predicted cls', cls_img)
+                    cv2.imshow('Predicted class', cls_img)
                     if cv2.waitKey(10) == 27:  # Клавиша Esc
                         break
 
@@ -111,7 +133,14 @@ def parse_args() -> argparse.Namespace:
         help=('Директория с изображениями предсказываемых классов. '
               'Если указана, то будет отображаться изображение '
               'предсказанного класса.'))
-    args = parser.parse_args()
+    parser.add_argument(
+        '--map_path', type=Path, default=None,
+        help=('Путь до изображения карты. Если указан, то классифицированный '
+              'регион будет отображаться на карте.'))
+    parser.add_argument(
+        '--step_px', type=int, default=224,
+        help=('Шаг окна в пикселях. '
+              'По умолчанию равен 224, то есть без перекрытия.'))
 
     if not args.input_dir.exists():
         raise FileNotFoundError(
