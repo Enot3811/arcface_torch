3c56cccbfbcba0169544a99ea90a89a07ed20775
Enot3811 <enot3811@gmail.com>
2023-06-08 17:00:07 +0300

    Конфиг 1000 окно 100 перекрытие 200 семплов 224 вход Кроп, аугмикс, блюр, джитер, кручения

---
+++ configs/real_data_r200_input224px_win1000m_overlap100m_samples200.py
@@ -0,0 +1,29 @@
+from easydict import EasyDict as edict
+
+# make training faster
+# our RAM is 256G
+# mount -t tmpfs -o size=140G  tmpfs /train_tmp
+
+config = edict()
+config.margin_list = (1.0, 0.5, 0.0)
+config.network = "r200"
+config.resume = True
+config.output = None
+config.embedding_size = 512
+config.sample_rate = 1.0
+config.fp16 = True
+config.momentum = 0.9
+config.weight_decay = 5e-4
+config.batch_size = 32
+config.lr = 0.01
+config.verbose = 2000
+config.dali = False
+
+config.rec = ('/home/pc0/projects/arcface/data/real_images_dataset/'
+              'win1000m_overlap100m_samples200_input224px')
+config.num_classes = 3906
+config.num_image = 3906 * 200
+config.num_epoch = 20
+config.warmup_epoch = 0
+config.val_targets = ['lfw', 'cfp_fp', "agedb_30"]
+config.save_all_states = True
